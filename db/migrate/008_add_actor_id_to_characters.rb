409aca101dc06f00d8895b74c74fb9b37fafe777
Eric McKinney <mckinney99@gmail.com>
2017-12-11 20:30:07 +0000

    automatically backed up by learn

--- db/migrate/008_add_actor_id_to_characters.rb
+++ db/migrate/008_add_actor_id_to_characters.rb
@@ -1,6 +1,6 @@
 class AddActorIdToCharacters < ActiveRecord::Migration[4.2]
 
   def change
-    create_column :characters, :actor_id, :integer
+    add_column :characters, :actor_id, :integer
   end
 end
